75e7a7b5d3288174db2c6dc73652983da02a4f6b
Marcos Garcia Puche <maarcosgarciapuche@gmail.com>
2026-02-22 12:51:27 +0100

    Control de errores

--- palindromo.py
+++ palindromo.py
@@ -1,14 +1,17 @@
-palabra = input("Introduce una palabra para poder comprobar si es un palíndromo: ")
+try:
+    palabra = str(input("Introduce una palabra para poder comprobar si es un palíndromo: "))
 
-# Para evitar mayusculas y espacios de más
-palabraCorrecta = palabra.lower().replace(" ", "")
+    # Para evitar mayusculas y espacios de más
+    palabraCorrecta = palabra.lower().replace(" ", "")
 
-palabraInvertida = ""
+    palabraInvertida = ""
 
-for letra in palabraCorrecta:
-    palabraInvertida = letra + palabraInvertida
+    for letra in palabraCorrecta:
+        palabraInvertida = letra + palabraInvertida
 
-if palabraCorrecta == palabraInvertida:
-    print("La palabra introducida es un palíndromo.")
-else: 
-    print("La palabra introducida no es palindromo.")
+    if palabraCorrecta == palabraInvertida:
+        print("La palabra introducida es un palíndromo.")
+    else: 
+        print("La palabra introducida no es palindromo.")
+except ValueError:
+    print("Error: no has introducido una palabra")
